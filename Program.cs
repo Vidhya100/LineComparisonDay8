138f708fa7307da523e73355685dfd661d5e5ca2
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-05 14:35:34 +0530

    Welcome message added

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using LineComparisonUsingObject;
+
+namespace LineComputation
+{
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            //Welcome message
+            Console.WriteLine("Welcome to line computation program.");
+
+            
+        }
+    }
+}
