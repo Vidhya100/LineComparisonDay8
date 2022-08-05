24e7a8b091044731ddab85d4c2e096bbc553e61f
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-05 14:44:05 +0530

    Length of line calculated

--- Program.cs
+++ Program.cs
@@ -13,7 +13,18 @@ namespace LineComputation
             //Welcome message
             Console.WriteLine("Welcome to line computation program.");
 
-            
+            //Taking line points from user
+            Console.WriteLine("Enter the line co-ordinates as x1,y1,x2,y2: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+           
+
+            //Calling Length of line function
+            LineComparison lineComparison = new LineComparison();
+            lineComparison.LengthOfLine(x1, y1, x2, y2);
         }
     }
 }
