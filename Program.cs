3a2f9e381c3e4245244118037aa77abd28db72c7
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-05 15:41:54 +0530

    Equality of lines checked using equals function

--- Program.cs
+++ Program.cs
@@ -20,11 +20,15 @@ namespace LineComputation
             double x2 = Convert.ToDouble(Console.ReadLine());
             double y2 = Convert.ToDouble(Console.ReadLine());
 
-           
+            Console.WriteLine("Enter the line co-ordinates as a1,b1,a2,b2: ");
+            double a1 = Convert.ToDouble(Console.ReadLine());
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            double a2 = Convert.ToDouble(Console.ReadLine());
+            double b2 = Convert.ToDouble(Console.ReadLine());
 
             //Calling Length of line function
             LineComparison lineComparison = new LineComparison();
-            lineComparison.LengthOfLine(x1, y1, x2, y2);
+            lineComparison.LengthOfLine(x1, y1, x2, y2, a1, b1, a2, b2);
         }
     }
 }
